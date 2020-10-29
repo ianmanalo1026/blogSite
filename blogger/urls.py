52abb7ecb0d9129e7609d38c06ab2a277c7af91b
Ian Manalo <69578172+ianmanalo1026@users.noreply.github.com>
2020-10-29 19:19:15 +0800

    Almost Complete

--- blogger/urls.py
+++ blogger/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     path('signupuser/', views.signupuser, name='signupuser'),
     path('logoutuser/', views.logoutuser, name='logoutuser'),
     path('loginuser/', views.loginuser, name='loginuser'),
-    path('homepage/', views.homepage, name='homepage'),
-    path('createblog/', views.createblog, name='createblog'),
-    path('<int:blog_pk>/edit', views.editblog, name='editblog'),
+    
+    path('profile/',views.profile, name='profile'),
+    path('profile/createblog/', views.createblog, name='createblog'),
+    path('profile/<int:blog_pk>/', views.profiledetail, name='profiledetail'),
+    path('profile/<int:blog_pk>/editblog/', views.editblog, name='editblog'),
+    path('profile/<int:blog_pk>/deleteblog/', views.deleteblog, name='deleteblog'),
 ]
--- blogger/views.py
+++ blogger/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .models import BlogPost
 from .forms import CreateBlogForm
+from django.contrib.auth.decorators import login_required
 
 def signupuser(request):
     if request.method == 'GET':
@@ -21,6 +22,7 @@ def signupuser(request):
         else:
             return render(request, 'blogger/signupuser.html', {'form':UserCreationForm(), 'error':'Password did not match!'})
         
+@login_required()
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
@@ -49,12 +51,17 @@ def detail(request, blog_pk):
     """
     User View
     """
-def homepage(request):
+@login_required()   
+def profile(request):
     content = BlogPost.objects.filter(blog_user=request.user)
-    return render(request, 'blogger/home.html', {'content':content})    
+    return render(request, 'blogger/profile.html', {'content':content})    
 
+@login_required()
+def profiledetail(request,blog_pk):
+    content = get_object_or_404(BlogPost, pk=blog_pk, blog_user=request.user)
+    return render(request, 'blogger/profiledetail.html', {'content':content})
 
-
+@login_required()
 def createblog(request):
     if request.method == 'GET':
         return render(request, 'blogger/createblog.html', {'form':CreateBlogForm()})
@@ -65,12 +72,25 @@ def createblog(request):
             #the .blog_user in blog.blog_user should be the same name on the .models
             blog.blog_user = request.user
             blog.save()
-            return redirect('home')
-            
+            return redirect('profile')
+        
+@login_required()          
 def editblog(request, blog_pk):
     content = get_object_or_404(BlogPost, pk=blog_pk, blog_user=request.user)
     if request.method == 'GET':
         form = CreateBlogForm(instance=content)
         return render(request, 'blogger/editblog.html', {'content':content, 'form':form})
     else:
-        pass
+        try:
+            form =  CreateBlogForm(request.POST, instance=content)
+            form.save()
+            return redirect('profile')
+        except ValueError:
+            return render(request, 'blogger/editblog.html', {'content':content, 'form':form, 'error':'Please Try Again! '})
+        
+@login_required()       
+def deleteblog(request, blog_pk):
+    content = get_object_or_404(BlogPost, pk=blog_pk, blog_user=request.user)
+    if request.method == "POST":
+        content.delete()
+        return redirect('profile')
