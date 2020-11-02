14bd3f00d2b1a43e88fa91b779e9a01acf45f248
Ian Manalo <69578172+ianmanalo1026@users.noreply.github.com>
2020-11-02 17:58:24 +0800

    connect the signals to App

--- blog2/settings.py
+++ blog2/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogger',
+    'blogger.apps.BloggerConfig',
     'crispy_forms'
 ]
 
